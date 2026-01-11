c09ad6e285fdda64443c9307da25f85ebd563412
NetZero <73232007+Rigava@users.noreply.github.com>
2026-01-11 14:43:32 +0530

    Update stockReader.py

--- stockReader.py
+++ stockReader.py
@@ -99,6 +99,7 @@ def main():
     st.title("📈 AI-Powered Technical Analysis")
     # symbol_list = ["RELIANCE", "SBIN","TCS","INFY","ITC"]
     tickers = pd.read_html('https://ournifty.com/stock-list-in-nse-fo-futures-and-options.html#:~:text=NSE%20F%26O%20Stock%20List%3A%20%20%20%20SL,%20%201000%20%2052%20more%20rows%20')[0]
+    tickers = tickers[5:]
     symbol_list = tickers.SYMBOL.to_list()
     symbol = st.selectbox("Select stock symbol", symbol_list)
     if symbol:
