5dfc3b9707102a2391f8451a07dcb33e435ac66d
NetZero <73232007+Rigava@users.noreply.github.com>
2026-04-12 11:19:20 +0530

    Update IndiaFairness.py

--- IndiaFairness.py
+++ IndiaFairness.py
@@ -100,9 +100,9 @@ def classify_regime(adx):
 
 jpn_tickers = [
   "RELIANCE.NS", "LTF.NS","BEL.NS","JIOFIN.NS","COCHINSHIP.NS","HUDCO.NS","IREDA.NS","ADANIENT.NS","MOTHERSON.NS","NTPC.NS",
-"ADANIGREEN.NS","IOC.NS","NMDC.NS","JSWINFRA.NS","IRFC.NS","VBL.NS","MARINE.NS","NCC.NS","IFCI.NS","RIBINFRA.NS"
+"ADANIGREEN.NS","IOC.NS","NMDC.NS","IRFC.NS","VBL.NS","MARINE.NS","NCC.NS","IFCI.NS","RIBINFRA.NS"
 ]
-removed=["IRCON.NS","DOLATALGO.NS","MAHABANK.NS","RITES.NS"]
+removed=["IRCON.NS","DOLATALGO.NS","MAHABANK.NS","RITES.NS","JSWINFRA.NS"]
 selected_tickers = st.sidebar.multiselect("Select your stocks",tickers,default = jpn_tickers,)
 # Streamlit app
 # st.set_page_config(layout="wide")
