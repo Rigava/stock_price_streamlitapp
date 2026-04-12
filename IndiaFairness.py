4c89e33fff3873b71864be0db50848f7f3315930
NetZero <73232007+Rigava@users.noreply.github.com>
2026-04-12 11:15:59 +0530

    Fix syntax error in selected_tickers assignment

--- IndiaFairness.py
+++ IndiaFairness.py
@@ -102,7 +102,7 @@ jpn_tickers = [
   "RELIANCE.NS", "LTF.NS","BEL.NS","JIOFIN.NS","COCHINSHIP.NS","HUDCO.NS","IREDA.NS","ADANIENT.NS","MOTHERSON.NS","NTPC.NS","IRCON.NS",
 "ADANIGREEN.NS","IOC.NS","DOLATALGO.NS","NMDC.NS","MAHABANK.NS","RITES.NS","JSWINFRA.NS","IRFC.NS","VBL.NS","MARINE.NS","NCC.NS","IFCI.NS","RIBINFRA.NS"
 ]
-selected_tickers = st.sidebar.multiselect("Select your stocks",tickers,default = jpn_tickers)
+selected_tickers = st.sidebar.multiselect("Select your stocks",tickers,default = jpn_tickers,)
 # Streamlit app
 # st.set_page_config(layout="wide")
 # --- PAGE SETUP ---
